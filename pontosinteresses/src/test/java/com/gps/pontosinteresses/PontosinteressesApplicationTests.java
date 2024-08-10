5d55de704716e011b316d27ee0dd2eb140de7efb
Alexandre Da Silva <alexandrerider@hotmail.com>
2024-08-10 10:49:21 -0300

    Primeiro commite

---
+++ pontosinteresses/src/test/java/com/gps/pontosinteresses/PontosinteressesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gps.pontosinteresses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontosinteressesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
